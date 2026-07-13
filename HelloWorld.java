713e7df53158a787da7cae1cc64e4a519116858b
iti1233 <129650889+iti1233@users.noreply.github.com>
2026-07-13 12:55:30 +0530

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+P1:- Write a program print hello world.
+solution :-
+class hello {
+    public static void main (String []k ){
+        System.out.print("Hello world ");
+    }
+    
+}
+output :- Hello world 
