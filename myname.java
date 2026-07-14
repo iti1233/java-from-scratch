f595e2ddb5e33bcd9200cce24e38f725ab229b3d
iti1233 <129650889+iti1233@users.noreply.github.com>
2026-07-14 13:18:58 +0530

    myname.java

---
+++ myname.java
@@ -0,0 +1,11 @@
+P2:-Write a program to print your name .
+solution
+  class myname
+{
+    public static void main(String []k)
+    {
+        System.out.print("Your name");
+    }
+}
+
+Note :- In it i use simpe way to print name without using scanner and buffer class.
