82ed81a94eb963d8154c7c93b9f1f97144cf277b
iti1233 <129650889+iti1233@users.noreply.github.com>
2026-07-15 12:41:05 +0530

    multipleline.java

---
+++ multipleline.java
@@ -0,0 +1,11 @@
+P3:- Write a program to print multiple lines.
+solution:-
+  class multipleline
+{
+    public static void main(String []k)
+    {
+        System.out.println("hello");
+        System.out.println("world");
+        System.out.println("try to print multiple line");
+    }
+}
